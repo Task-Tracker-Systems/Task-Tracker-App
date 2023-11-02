8f956cec28335449a27f5a76f15cdefc08e5c1c2
Gerrit Garbereder <info@garbereder.com>
2023-11-02 07:05:57 +0200

    Rename sqlite packages

--- Task-Tracker-Usecases-SQLite-Impl/src/nativeMain/kotlin/com/garbereder/tasktracker/usecases/tasks/DBTaskCollectionReaderFactory.kt
+++
@@ -1,5 +0,0 @@
-package com.garbereder.tasktracker.usecases.tasks
-
-actual class DBTaskCollectionReaderFactory(private val factory: DBDriverFactory) : TaskCollectionReaderFactory {
-    actual override fun create(): TaskCollectionReader = DBTaskCollectionReader(factory)
-}
