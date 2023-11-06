e83efc37f9c6a3fe4d14b391204b817190287193
Gerrit Garbereder <gerrit@garbereder.com>
2023-11-06 22:04:20 +0200

    Add module dependencies

--- Task-Tracker-GUI/desktop/build.gradle.kts
+++ Task-Tracker-GUI/desktop/build.gradle.kts
@@ -32,6 +32,7 @@ compose.desktop {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "TaskTrackerGUI"
             packageVersion = "1.0.0"
+            modules("java.instrument", "java.sql", "jdk.unsupported")
             buildTypes.release {
                 proguard {
                     configurationFiles.from("compose.pro")
